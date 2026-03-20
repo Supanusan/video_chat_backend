01857044f5b450696847b0b5db7b7bee8c40532a
Supanusan <ssupanusan@gmail.com>
2026-03-20 08:38:18 +0530

    Change server port from 3001 to 8080

--- server.js
+++ server.js
@@ -139,8 +139,8 @@ io.on('connection', (socket) => {
 //   .then(() => console.log('MongoDB connected'))
 //   .catch(err => console.error('MongoDB error:', err));
 
-const PORT = process.env.PORT || 3001;
-// ✅ Fixed
+const PORT = process.env.PORT || 8080;
+
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
 });
